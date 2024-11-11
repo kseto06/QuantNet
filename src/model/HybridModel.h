e3b615c8acbccb4d62b1f5db0265764727a2c17d
Kaden Seto | 司徒逸希 <127336883+kseto06@users.noreply.github.com>
2024-11-11 13:46:35 -0500

    feat: minibatch generation
    minibatch generation for X (Tensor3D) and Y (m by 1 Matrix)

--- src/model/HybridModel.h
+++ src/model/HybridModel.h
@@ -6,7 +6,9 @@
 namespace HybridModel {
     typedef std::vector<std::vector<double>> Matrix;
     typedef std::vector<std::vector<std::vector<double>>> Tensor3D;
-    
+    typedef std::tuple<Tensor3D, Matrix> minibatch;
+
+    std::vector<minibatch> generate_minibatches(Tensor3D X, Matrix Y, int batch_size, int seed);
     double MSE(const std::vector<double>& pred, const std::vector<double>& target);
     void init_layers(const std::vector<std::string>& layer_type, const std::vector<int>& layer_dim);
     void initialize_network();
